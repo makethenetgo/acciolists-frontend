d2ddb3ee970150cd8b08072eeea47fcd2ca83f9e
DN <DN@DN.local>
2024-12-20 22:31:18 -0500

    runes.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
